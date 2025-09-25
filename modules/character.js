38c61c4c2f88d85c97d2395a6c88adcefd2af6c5
Caprican <CapricanDRJ@users.noreply.github.com>
2025-09-25 16:36:47 -0400

    test

--- modules/character.js
+++ modules/character.js
@@ -100,7 +100,10 @@ module.exports = {
 
                     setTimeout(async () => {
                         // Get avatar data
-                        const avatarURL = interaction.user.displayAvatarURL({ extension: 'png', size: 64, dynamic: false});
+                        const avatarURL = interaction.user.displayAvatarURL()
+                        .replace(/\/a_/, '/')
+                        .replace(/\.[a-zA-Z]{3,4}$/, '')
+                        + '.png?size=64';
 
                         console.log(avatarURL);
                         const avatarFileName = avatarURL.split('/').pop().split('?')[0];
