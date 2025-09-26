e15e957a4f1603077f06900a822c050c0b6b067d
Caprican <CapricanDRJ@users.noreply.github.com>
2025-09-25 21:58:42 -0400

    miniquest

---
+++ modules/miniquest.js
@@ -0,0 +1,12 @@
+const randomHealerQuest = [
+  ["The Ever-Full Flask 🥤", "Thy cup shall never run dry! Thou dost remember to hydrate more frequently!"],
+  ["The Cozy Cloak of Softness 🧣", "Thy sensory needs are met! Thou dost gain comfort & warmth bonuses for deep relaxation."],
+  ["The Windborne Muse 💨", "Inspiration strikes!"],
+  ["The Weighted Blanket of Tranquility 🛏", "Thine anxieties are eased! Thou shalt fall asleep more swiftly and rest more deeply."],
+  ["The Arcane Spice Rack 🌿", "Thy meals taste superior!"],
+  ["The Enchanted Candle of Focus 🕯", "Boosts concentration & ambiance!"],
+  ["The Lavender Satchel of Peace 🌸", "Repels stress! Thou dost gain a resistance against distractions & sensory overload!"],
+  ["The Satchel of Perpetual Order 🎒", "Reduces clutter! Next organizational task takes half the effort."],
+  ["The Starlit Headphones 🎧", "Protects thee from auditory distractions! Focus tasks are less likely to be interrupted!"],
+  ["The Cozy Cocoon Socks 🧦", "Feet are warm, mind is at peace! Sitting still during study sessions becomes twice as easy."]
+];
