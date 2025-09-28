07dd19da62d314994c7b6f65911a4abd8ee4c65d
Caprican <CapricanDRJ@users.noreply.github.com>
2025-09-28 18:22:56 -0400

    formatting

--- modules/quest.js
+++ modules/quest.js
@@ -289,8 +289,8 @@ module.exports = {
             for (const item of activeItems) {
               skillBonuses[item.skill - 1] = item.skillBonus;
               professionBonuses[item.professionId - 1] = item.professionBonus;
-              itemString += item.skillBonus ? `\n- <:emoji:${item.emojiId}> ${item.name}\n - - ${skillNames[user.domainId - 1][item.skill - 1]} Bonus X${item.skillBonus}` : '';
-              itemString += item.professionBonus ? `\n- <:emoji:${item.emojiId}> ${item.name}\n - - ${professionNames[parseInt(item.professionId) - 1]} +${item.professionBonus}` : '';
+              itemString += item.skillBonus ? `\n- <:emoji:${item.emojiId}> ${item.name}\n - - ${skillNames[user.domainId - 1][item.skill - 1]} +${item.skillBonus}` : '';
+              itemString += item.professionBonus ? `\n- <:emoji:${item.emojiId}> ${item.name}\n - - ${professionNames[parseInt(item.professionId) - 1]} X${item.professionBonus}` : '';
             }
             db.prepare(`UPDATE users SET skill1 = skill1 + ?, skill2 = skill2 + ?, skill3 = skill3 + ?, skill4 = skill4 + ?, skill5 = skill5 + ?, skill6 = skill6 + ?, coins = coins + ?, ${profession} = ${profession} + ? WHERE userId = ? AND guildId = ?`)
             .run(quest.skill1, quest.skill2, quest.skill3 , quest.skill4, quest.skill5, quest.skill6, quest.coins, quest.professionXp * professionBonuses[parseInt(quest.professionId) - 1], interaction.user.id, interaction.guildId);
