9555305d6aeed5f852455f5972a1c7f8cf04ddb8
Caprican <CapricanDRJ@users.noreply.github.com>
2025-09-27 23:04:53 -0400

    fix

--- modules/stats.js
+++ modules/stats.js
@@ -328,9 +328,9 @@ module.exports = {
             const imageBuffer = await generateCharacterImage(userData, domainData, userData.avatar);
             const fileName = `${userData.displayName}-stats.png`;
             const attachment = new AttachmentBuilder(imageBuffer, { name: fileName });
-            const embeds = new EmbedBuilder()
+            const embeds = [new EmbedBuilder()
                 .setImage(`attachment://${fileName}`)
-                .setColor(domainData.background);
+                .setColor(domainData.background)];
 
             interaction.reply({
                 embeds,
