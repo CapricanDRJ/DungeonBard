59c3924e10be0e5d7609517301b27b107db05ca8
Caprican <CapricanDRJ@users.noreply.github.com>
2025-09-30 15:12:36 -0400

    test

--- modules/scoreboard.js
+++ modules/scoreboard.js
@@ -156,7 +156,8 @@ module.exports = {
 
     executeCommand: async (interaction) => {
         const userId = interaction.user.id;
-        const guildId = interaction.guildId;
+//        const guildId = interaction.guildId;
+        const guildId = '1339984756695371908';
 
         try {
             // Get all users with avatars in a single query
