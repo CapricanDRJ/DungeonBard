49e124fe2238d464ebe9d1f54729383a6ea2cdd6
Caprican <CapricanDRJ@users.noreply.github.com>
2026-04-10 10:26:22 -0400

    fix: adjust banner top positioning in scoreboard image generation

--- modules/scoreboard.js
+++ modules/scoreboard.js
@@ -289,7 +289,7 @@ async function generateScoreboardImage(users, gainUsers, highlightIndex, rank = 
                 rank = 1;
                 plus = '+';
                 const bannerLeft = Math.floor((bgWidth - h1Width) / 2);
-                const bannerTop = y - gainOffset;
+                const bannerTop = y - 2 - gainOffset;
                 compositeLayers.push({
                     input: hrBuffer,
                     top: bannerTop,
