9c0c876280d835ce2adbb49bd52c985ce8590cb4
Caprican <CapricanDRJ@users.noreply.github.com>
2026-06-11 19:01:52 -0400

    fix: allow null embeds in message data to prevent errors in message handling

--- modules/quest.js
+++ modules/quest.js
@@ -314,7 +314,7 @@ async function menu(interaction, isUpdate, stage = 1, selectedArea = null, selec
     }
 
     const messageData = {
-      embeds: [embed],
+      embeds: [embed, null].filter(Boolean),
       components: components,
       flags: MessageFlags.Ephemeral
     };
